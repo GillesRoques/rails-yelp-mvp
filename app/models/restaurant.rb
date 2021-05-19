0850870aae687fa3f728bbdec5883c89aca4f3b8
Gilles Roques <gilles.roques@gmail.com>
2021-05-19 17:06:36 +0200

    yelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,9 @@
 class Restaurant < ApplicationRecord
-
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese french italian japanese belgian] }
 
 
 end
